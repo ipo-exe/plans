f23c65c5ca168f59c1004eb96edaf06a8d48ec79
ipo-exe <possantti@gmail.com>
2023-03-06 18:46:11 -0300

    new objects

--- plans/datasets.py
+++ plans/datasets.py
@@ -1,14 +1,102 @@
 """
-Datasets - Python library for cooks and food lovers.
-This is a Python docstring, we can use reStructuredText syntax here!
-.. code-block:: python
+
+This module provides a set of dataset objects for input, output and processing.
+
+::
     # Import Datasets
     import datasets
     # Call its only function
     datasets.my_function(kind=["cheeses"])
+
+This file is part of the PLANS project.
+Licensed under the GNU GENERAL PUBLIC LICENSE (GPL 3).
+For more information, see https://github.com/ipo-exe/plans
+
 """
 
 __version__ = "0.1.0"
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class DailySeries:
+    """
+    The basic daily time series object
+
+    Example of using this object:
+
+    """
+
+    def __init__(self, name, file, varfield, datefield, location):
+        '''
+        Deploy the basic Time Series object
+
+        :param name: Time Series name
+        :type name: str
+        :param file: dataset path to `.txt` CSV file (separator = ;)
+        :type file: str
+        :param varfield: name of the variable field in file
+        :type varfield: str
+        :param datefield: name of date field in file
+        :type datefield: str
+        :param location: dictionary of `lat`, `long` and `CRS` of measuring station
+        :type location: dict
+        '''
+        # -------------------------------------
+        # set basic attributes
+        self.name = name
+        self.file = file
+        self.varfield = varfield
+        self.datefield = datefield
+        self.location = location
+
+        # -------------------------------------
+        # import data
+        df_aux = pd.read_csv(
+            self.file,
+            sep=";",
+            parse_dates=[self.datefield]
+        )
+        # slice only varfield and datefield
+        self.data = df_aux[[self.datefield, self.varfield]]
+
+
+    def __str__(self):
+        s_aux = "\n{}\n{}\n{}\n".format(
+            self.name,
+            self.location,
+            self.data
+        )
+        return s_aux
+
+
+    def _dummy(self):
+        return self.data[self.varfield].mean()
+
+
+    def resample_sum(self, period):
+        pass
+
+    def resample_mean(self, period):
+        pass
+
+
+
+class PrecipitationSeries(DailySeries):
+
+    def new_method(self):
+        return self.data[self.varfield].sum()
+
+
+class TemperatureSeries(DailySeries):
+    pass
+
+class StreamflowSeries(DailySeries):
+    pass
+
+
 
 
 class MyClass:
@@ -50,3 +138,38 @@ def my_function(kind=None):
     :rtype: list[str]
     """
     return ["shells", "gorgonzola", "parsley"]
+
+
+if __name__ == '__main__':
+
+
+    ts = DailySeries(
+        name="MyTS",
+        file="C:/bin/calib_series.txt",
+        varfield="Prec",
+        datefield="Date",
+        location={
+            "lat": -30.0,
+            "long": -51.0,
+            "CRS": "SIRGAS 2000"
+        }
+    )
+
+    print(ts)
+    n = ts._dummy()
+    print(n)
+
+    ts_p = PrecipitationSeries(
+        name="MyTS",
+        file="C:/bin/calib_series.txt",
+        varfield="Prec",
+        datefield="Date",
+        location={
+            "lat": -30.0,
+            "long": -51.0,
+            "CRS": "SIRGAS 2000"
+        }
+    )
+    print(ts_p.data)
+    print(ts_p._dummy())
+    print(ts_p.new_method())
