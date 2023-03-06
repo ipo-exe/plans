88d383f89cb86441880989fae657b3c18a3d1804
Iporã Possantti <61351163+ipo-exe@users.noreply.github.com>
2023-03-06 11:18:30 -0300

    Create __init__.py

---
+++ plans/__init__.py
@@ -0,0 +1,9 @@
+"""
+Routines for working with triangles.
+The two modules inside of this package are packed with useful features
+for the programmer who needs to support triangles:
+
+``datasets``
+    This module provides objects to handle all PLANS datasets.
+ 
+"""
